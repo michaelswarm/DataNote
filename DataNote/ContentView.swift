fffdb1fe4a5642af79307f1358539d9a9f0ead68
michaelswarm <52172564+michaelswarm@users.noreply.github.com>
2025-03-17 08:36:31 -0600

    Wiki  support in ContentView.

--- DataNote/ContentView.swift
+++ DataNote/ContentView.swift
@@ -21,6 +21,37 @@ struct ContentView: View {
     @State var isExpanded = false
     @State var searchType: SearchType = .title
     @Environment(ExportModel.self) var bulkModel
+    
+    // Wiki Model (depends of query, update? performance-scale?) Can model update be driven by view on change??? 
+    @State var selectedTitle: String?
+    @State var contentSelection: String? // TBD: Optionally used for add title or scroll to content selection...
+    var titles: [String] { notes.map(\.title) }
+    var titlesExcludingSelf: [String] {
+        if let selectedTitle = selectedTitle {
+            var set = Set(titles)
+            if let removed = set.remove(selectedTitle) {
+                return Array(set)
+            } else {
+                return []
+            }
+        } else {
+            return []
+        }
+    }
+    var titlesExcludingSelfSorted: [String] {
+        titlesExcludingSelf.sorted { $0.count < $1.count }
+    }
+    func resolveNote(from title: String?) -> Note? {
+        guard let title = title else { return nil }
+        
+        if let index = notes.firstIndex { $0.title == title } {
+            let note = notes[index]
+            return note
+        } else {
+            print("ResolveNote: Can not find index...")
+            return nil
+        }
+    }
 
     // The sortDescriptor value is passed separately from sortOption binding, so that re-init and re-query whenever sort descriptor changes. A sortOption change just re-calculates the body (sidebar).
     init(sortDescriptor: SortDescriptor<Note>, sortOption: Binding<SortOption>, config: StorageConfiguration, selection: Binding<Note?>) {
@@ -31,6 +62,7 @@ struct ContentView: View {
     }
 
     var body: some View {
+        
         NavigationSplitView {
             VStack(spacing: 0) {
                 SearchBar(isExpanded: $isExpanded, searchText: $searchText, searchType: $searchType)
@@ -86,6 +118,24 @@ struct ContentView: View {
         } detail: {
             if let selectedNote = selectedNote {
                 NoteDetailView(note: selectedNote)
+                
+                // Wiki: Can not be in model unless model can query and update notes. Workaround use view on change.
+                    .onChange(of: selectedTitle) { oldValue, newValue in
+                        // Resolve title to note
+                        self.selectedNote = resolveNote(from: newValue) // After update, before value changes.
+                    }
+                /*let noteTitle = Binding(
+                    get: { note.title },
+                    set: { note.update(keyPath: \.title, to: $0) }
+                )*/
+                let noteContent = Binding(
+                    get: { selectedNote.content },
+                    set: { selectedNote.update(keyPath: \.content, to: $0) }
+                )
+                // WikiEditor here...
+                // WikiEditor(text: noteContent, title: $selectedTitle, titlesShortestFirstExcludingSelf: titlesExcludingSelfSorted, selection: $contentSelection)
+
+                
             } else {
                 Text("Select a note")
             }
